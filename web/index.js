c3ced2b0f896c50fd1103d14df87becbac46e4de
Hope itsforce <hope@itsforce.com>
2024-10-16 11:39:16 +0700

    update code

--- web/index.js
+++ web/index.js
@@ -1,61 +1,7 @@
-// @ts-check
-import { join } from "path";
-import { readFileSync } from "fs";
-import express from "express";
-import serveStatic from "serve-static";
-
-import mongoose, { Schema } from 'mongoose';
-
 import shopify from "./shopify.js";
-import GDPRWebhookHandlers from "./gdpr.js";
-
-const PORT = parseInt(
-  process.env.BACKEND_PORT || process.env.PORT || "3000",
-  10
-);
-
-const STATIC_PATH =
-  process.env.NODE_ENV === "production"
-    ? `${process.cwd()}/frontend/dist`
-    : `${process.cwd()}/frontend/`;
-
-const app = express();
-
-// Set up Shopify authentication and webhook handling
-app.get("/api/auth", shopify.auth.begin());
-app.get("/api/auth/callback", shopify.auth.callback(), shopify.redirectToShopifyOrAppRoot());
-
-app.post(
-  shopify.config.webhooks.path,
-  shopify.processWebhooks({ webhookHandlers: GDPRWebhookHandlers })
-);
-
-// If you are adding routes outside of the /api path, remember to
-// also add a proxy rule for them in web/frontend/vite.config.js
 
 app.use("/api/*", shopify.validateAuthenticatedSession());
 
-app.use(express.json());
-
-
-
-//Api
-
-app.use(shopify.cspHeaders());
-app.use(serveStatic(STATIC_PATH, { index: false }));
-
-app.use("/*", shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
-  return res
-    .status(200)
-    .set("Content-Type", "text/html")
-    .send(readFileSync(join(STATIC_PATH, "index.html")));
-});
-
-mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://lyonikgeo:cYRoZyEpBuiaru9x@cluster0.jjyz5.mongodb.net/inventoryapp')
-.then(result => {
-	app.listen(PORT);
-})
-.catch(err => {
-	console.log(err);
-})
+app.get("/api/auth", shopify.auth.begin());
+app.get("/api/auth/callback", shopify.auth.callback(), shopify.redirectToShopifyOrAppRoot());
+app.post(shopify.config.webhooks.path, shopify.processWebhooks());
--- web/shopify.js
+++ web/shopify.js
@@ -1,52 +1,17 @@
-import { BillingInterval, BillingReplacementBehavior, LATEST_API_VERSION } from "@shopify/shopify-api";
-import { shopifyApp } from "@shopify/shopify-app-express";
-import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
-import { MongoDBSessionStorage } from '@shopify/shopify-app-session-storage-mongodb';
-import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
-
-const DB_PATH = `${process.cwd()}/database.sqlite`;
-
-// The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
-// See the ensureBilling helper to learn more about billing in this template.
-const billingConfig = {
-  "MONTHLY": {
-    interval: BillingInterval.Every30Days,
-    amount: 29.98,
-    currencyCode: "USD",
-    trialDays: 10,
-    replacementBehavior: BillingReplacementBehavior.ApplyImmediately,
-    test: true,
-  },
-  "USAGE": {
-    interval: BillingInterval.Usage,
-    amount: 50,
-    currencyCode: "USD",
-    trialDays: 7,
-    replacementBehavior: BillingReplacementBehavior.ApplyImmediately,
-    test: true,
-    usageTerms: "$0.01 per order per month"
-  }
-};
+import { shopifyApp } from '@shopify/shopify-app-express';
+import { LATEST_API_VERSION } from '@shopify/shopify-api';
+import shopify from "./shopify.js";
 
 const shopify = shopifyApp({
   api: {
     apiVersion: LATEST_API_VERSION,
-    restResources,
-    billing: billingConfig, // or replace with billingConfig above to enable example billing
-    scopes: ['read_products','write_products','write_content','write_inventory','read_inventory','read_price_rules','write_price_rules','read_themes','write_themes']
-  },
-  auth: {
-    path: "/api/auth",
-    callbackPath: "/api/auth/callback",
+    scopes: ['read_products', 'write_products'],
   },
   webhooks: {
     path: "/api/webhooks",
-  },
-  // This should be replaced with your preferred storage strategy
-  sessionStorage: new MongoDBSessionStorage(
-    'mongodb+srv://lyonikgeo:cYRoZyEpBuiaru9x@cluster0.jjyz5.mongodb.net',
-    'inventoryapp',
-  )
+    webhookHandlers: {
+    },
+  }
 });
 
 export default shopify;
